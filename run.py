47b12969cf52912622cd77f78f3f4fc04961e4e0
Gaeros <163143799+elias-gaeros@users.noreply.github.com>
2024-08-16 22:40:33 +0200

    allow overloading env var

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 from typing import Union, OrderedDict
 from dotenv import load_dotenv
 # Load the .env file if it exists
-load_dotenv()
+load_dotenv(override=True)
 
 sys.path.insert(0, os.getcwd())
 # must come before ANY torch or fastai imports
